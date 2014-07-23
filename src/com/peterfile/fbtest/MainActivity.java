1610eedcecb83700df7516cb12e412be091b7055
Mark File <Mark@HappySale.com>
2014-07-23 11:51:00 +0200

    Personalize, display user photo and name.

--- src/com/peterfile/fbtest/MainActivity.java
+++ src/com/peterfile/fbtest/MainActivity.java
@@ -1,15 +1,23 @@
 package com.peterfile.fbtest;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -29,6 +37,7 @@ public class MainActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		Log.w(TAG, "onCreate");
+		printKeyHash();
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
@@ -168,4 +177,21 @@ public class MainActivity extends FragmentActivity {
 		return false;
 	}
 
+	private void printKeyHash(){
+	    // Add code to print out the key hash
+	    try {
+	        PackageInfo info = getPackageManager().getPackageInfo(
+	                "com.peterfile.fbtest", 
+	                PackageManager.GET_SIGNATURES);
+	        for (Signature signature : info.signatures) {
+	            MessageDigest md = MessageDigest.getInstance("SHA");
+	            md.update(signature.toByteArray());
+	            Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+	            }
+	    } catch (NameNotFoundException e) {
+	        Log.d("KeyHash:", e.toString());
+	    } catch (NoSuchAlgorithmException e) {
+	        Log.d("KeyHash:", e.toString());
+	    }
+	}
 }
